a4da317154447fde0f03c04918defdd309ee02a9
Prabu D <prabud@spritle.com>
2014-04-16 19:25:06 +0530

    Rails login page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3Deployments::Application.config.secret_token = '66e5c35fb7d1d564294f144d2e3330b5f2825d25e7df673d65f78e62da4a50a0e6f0b69d72e3c93d5db71fd56b5d6f66598bed84503e27738a934848ab731068'
